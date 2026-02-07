f3b08950bf66a2246c12ec77f313719e9dbb173e
Jeongwoo Park <wahoo9040@naver.com>
2026-02-07 18:20:09 +0900

    feat: Note controller 구현 추가

---
+++ apps/server/src/plugins/note/note.controller.ts
@@ -0,0 +1,56 @@
+import type { FastifyRequest, FastifyReply } from "fastify";
+import type { NoteService } from "./note.service.js";
+import type {
+  CreateNoteRequest,
+  UpdateNoteRequest,
+} from "@/dtos/note/request.dto.js";
+
+export const createNoteController = (noteService: NoteService) => ({
+  getNotes: async (request: FastifyRequest, reply: FastifyReply) => {
+    const userId = request.user.id;
+    const notes = await noteService.getNotes(userId);
+    return reply.send(notes);
+  },
+
+  getNote: async (
+    request: FastifyRequest<{ Params: { id: string } }>,
+    reply: FastifyReply,
+  ) => {
+    const userId = request.user.id;
+    const { id } = request.params;
+    const note = await noteService.getNote(userId, id);
+    return reply.send(note);
+  },
+
+  createNote: async (
+    request: FastifyRequest<{ Body: CreateNoteRequest }>,
+    reply: FastifyReply,
+  ) => {
+    const userId = request.user.id;
+    const note = await noteService.createNote(userId, request.body);
+    return reply.code(201).send(note);
+  },
+
+  updateNote: async (
+    request: FastifyRequest<{
+      Params: { id: string };
+      Body: UpdateNoteRequest;
+    }>,
+    reply: FastifyReply,
+  ) => {
+    const userId = request.user.id;
+    const { id } = request.params;
+    const note = await noteService.updateNote(userId, id, request.body);
+    return reply.send(note);
+  },
+
+  deleteNote: async (
+    request: FastifyRequest<{ Params: { id: string } }>,
+    reply: FastifyReply,
+  ) => {
+    const userId = request.user.id;
+    const { id } = request.params;
+    await noteService.deleteNote(userId, id);
+    return reply.code(204).send();
+  },
+});
