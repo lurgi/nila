aa6eed3e4e9d5dd4b5591eeb049e420a93d30b40
Jeongwoo Park <wahoo9040@naver.com>
2026-02-07 10:37:25 +0900

    feat: 인증 스키마 추가 - 로그인, 토큰 응답, 리프레시 및 로그아웃 구조 정의

---
+++ apps/server/src/types/schemas/auth.schema.ts
@@ -0,0 +1,41 @@
+import { Type } from "@sinclair/typebox";
+import { AuthProvider } from "@/generated/prisma/client.js";
+import { UserSchema } from "./user.schema.js";
+
+export const LoginBodySchema = Type.Object({
+  provider: Type.Enum(AuthProvider),
+  providerId: Type.String(),
+  email: Type.String({ format: "email" }),
+  name: Type.Optional(Type.String()),
+  profileImage: Type.Optional(Type.String()),
+});
+
+export const TokenResponseSchema = Type.Object({
+  accessToken: Type.String(),
+  refreshToken: Type.String(),
+  user: UserSchema,
+});
+
+export const RefreshBodySchema = Type.Object({
+  refreshToken: Type.String(),
+});
+
+export const LoginSchema = {
+  body: LoginBodySchema,
+  response: {
+    200: TokenResponseSchema,
+  },
+};
+
+export const RefreshSchema = {
+  body: RefreshBodySchema,
+  response: {
+    200: TokenResponseSchema,
+  },
+};
+
+export const LogoutSchema = {
+  response: {
+    204: Type.Null(),
+  },
+};
